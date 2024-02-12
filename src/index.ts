5298b1214a7228e7039e4fb06f3ae306a7579b93
Duxcoder <iinegve_25@mail.ru>
2024-02-12 14:52:29 +0300

    feat: create get endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,87 @@
 import * as http from 'node:http';
-const users = [
-  { id: 1, name: 'John Doe' },
-  { id: 2, name: 'Jane Smith' },
-  { id: 3, name: 'Bob Johnson' },
-];
+import { v4 as uuidv4, validate } from 'uuid';
 
-const server = http.createServer((request, response) => {
-  // Handle GET request to /api/users
-  if (request.method === 'GET' && request.url === '/api/users') {
-    response.writeHead(200, {
-      'Content-Type': 'application/json',
-    });
-    response.end(JSON.stringify(users));
+console.log(uuidv4());
+interface User {
+  id: string;
+  username: string;
+  age: string;
+  hobbies: string[];
+}
+interface Users {
+  [key: string]: User;
+}
+
+interface Result {
+  status: 400 | 404 | 200;
+  content: string | User | Users;
+}
+
+const users: Users = {
+  '8b056444-b6b3-46d4-acd9-416c119c5293': {
+    id: '8b056444-b6b3-46d4-acd9-416c119c5293',
+    username: 'Duxcoder',
+    age: '12',
+    hobbies: ['js'],
+  },
+
+  '75ba0f01-b739-4b63-a54e-b07d2241f796': {
+    id: '75ba0f01-b739-4b63-a54e-b07d2241f796',
+    username: 'Guest',
+    age: '98',
+    hobbies: ['cartoon'],
+  },
+};
+
+const checkPathUser = (path: string) => {
+  return path.split('/').length === 4 && path.startsWith('/api/users/');
+};
+
+const getUserById = (id: string): Result => {
+  if (!validate(id)) return { status: 400, content: 'User id is invalid' };
+  if (id in users) {
+    return { status: 200, content: users[id] as User };
   } else {
-    // Handle other requests
-    response.writeHead(404);
-    response.end();
+    return { status: 404, content: 'User is not found' };
   }
+};
+
+const getRequests = (path: string = ''): Result => {
+  const request = path.split('/');
+  let id = '';
+  if (request.length === 4) id = request[3] as string;
+
+  switch (true) {
+    case path === '/api/users':
+      return { status: 200, content: users };
+    case checkPathUser(path):
+      return getUserById(id);
+  }
+  return { status: 404, content: 'Page is not found' };
+};
+
+const server = http.createServer((req, res) => {
+  let result: Result = { status: 404, content: 'Page is not found' };
+  switch (req.method) {
+    case 'GET':
+      result = getRequests(req.url?.trim());
+      break;
+    case 'POST':
+      // getRequests(req.url?.trim());
+      break;
+    case 'PUT':
+      // getRequests(req.url?.trim());
+      break;
+    case 'DELETE':
+      // getRequests(req.url?.trim());
+      break;
+  }
+
+  res.writeHead(result.status, {
+    'Content-Type': 'application/json',
+  });
+  res.statusCode = result.status;
+  res.end(JSON.stringify(result.content));
 });
 
 server.listen(8080, () => {
